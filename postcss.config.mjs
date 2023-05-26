f11e35f37b0cbcb8f663a22fd067e6ca39c4e82d
h-ris <95009925+h-ris@users.noreply.github.com>
2023-05-26 16:19:55 -0400

    Update and rename postcss.config.js to postcss.config.mjs

---
+++ postcss.config.mjs
@@ -0,0 +1,21 @@
+// If you want to use other PostCSS plugins, see the following:
+// https://tailwindcss.com/docs/using-with-preprocessors
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+
+/**
+   * @type {import('next').NextConfig}
+   */
+const nextConfig   = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+};
+
+export default   nextConfig;
